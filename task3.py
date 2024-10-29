9f0430d480301e064a8c9520d490902b41b9b16a
Khira <monayaa1637@gmail.com>
2024-10-29 08:50:20 +0300

    passed 2 tests

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from functions import *
-from task3_files import QuanTest1
+from task3_files import QuanTest1, QuanTest2 
 import math
 
 
@@ -33,13 +33,25 @@ class Task3:
         self.levels_entry = ttk.Entry(self.frame, textvariable=self.num_of_levels)
         self.levels_entry.grid(row=3, column=1, pady=5)
 
+        # Radio buttons to select sine or cosine
+        self.test = tk.StringVar(value="Test1")
+        tk.Radiobutton(self.frame, text="Test 1", variable=self.test, value="Test1").grid(row=4, column=0, pady=5)
+        tk.Radiobutton(self.frame, text="Test 2", variable=self.test, value="Test2").grid(row=4, column=1, pady=5)
+
         # Generate the signal
-        generate_button = ttk.Button(self.frame, text="Generate Signal", command=self.quantize_and_encode)
+        generate_button = ttk.Button(self.frame, text="Generate Signal", command=self.choose_test)
         generate_button.grid(row=6, column=0, columnspan=2, pady=10)
 
         self.intervals = dict()
         self.signal = None
-        self.binary_numbers = None
+        self.midpoints = []
+        #LISTS:
+        #intervals[index] = [(start,end)]
+        #encoded values [00, 01, 10, 11]
+        #quantized values [0.85, 0.75, 0.55 ..]
+        #errors
+        #intervals
+
 
     def display(self):
         self.frame.grid(row=1, column=0, columnspan=2, sticky='nsew')
@@ -48,75 +60,122 @@ class Task3:
         self.frame.grid_forget()
 
 
-    def quantize_and_encode(self):
-        self.signal = functions.read_signals("task3_files\\Quan1_input.txt")
+    def choose_test(self):
 
-        self.claculate_intervals()
-        quantized_values = self.quantize_values()
-        self.get_binary_numbers(self.num_of_bits.get())
-        encoded_values = self.encode_values()
-        self.plot_signal()
-        self.print_resutls(encoded_values, quantized_values)
-
-        QuanTest1.QuantizationTest1('task3_files\\Quan1_Out.txt', encoded_values, quantized_values)
+        test = self.test.get()
+        levels = self.num_of_levels.get()
+        bits = self.num_of_bits.get()
 
 
-    def claculate_intervals(self):
-        self.intervals.clear()
-        
-        if self.num_of_levels.get() == 0:  
-            self.num_of_levels.set(math.pow(2, int(self.num_of_bits.get())))
+        if levels == 0:  
+            self.num_of_levels.set(math.pow(2, int(bits))) #display it in text box
+            levels = self.num_of_levels.get()
 
-        if self.num_of_bits.get() == 0:  
-            self.num_of_bits.set(math.log2(int(self.num_of_levels.get())))
+        if bits == 0:  
+            self.num_of_bits.set(math.log2(int(levels)))
+            bits = self.num_of_bits.get()
+
+
+
+        #DRY, still thinking of a name to exract these in a function
+        if(test == "Test1"):
+            self.signal = functions.read_signals("task3_files\\Quan1_input.txt")  
+            intervals = self.get_intervals(levels)
+            quantized_values, errors = self.quantize_values(intervals)
+            binary_numbers = self.get_binary_numbers(bits)
+            intervals_indecies, encoded_values = self.encode_values(binary_numbers, quantized_values)
+
+            self.plot_signal()
+            self.print_resutls(intervals_indecies,encoded_values,quantized_values,errors)
+            QuanTest1.QuantizationTest1('task3_files\\Quan1_Out.txt', encoded_values, quantized_values)
+
+
+
+
+        else:
+            self.signal = functions.read_signals("task3_files\\Quan2_input.txt")
+            
+            intervals = self.get_intervals(levels)
+            quantized_values, errors = self.quantize_values(intervals)
+            binary_numbers = self.get_binary_numbers(bits)
+            intervals_indecies, encoded_values = self.encode_values(binary_numbers, quantized_values)
+
+            self.plot_signal()
+            self.print_resutls(intervals_indecies,encoded_values,quantized_values,errors)
+            QuanTest2.QuantizationTest2('task3_files\\Quan2_Out.txt',intervals_indecies,encoded_values,quantized_values,errors)
+
+
+        self.num_of_levels.set(0) #reset
+        self.num_of_bits.set(0) #reset
+        self.midpoints.clear()
+
+
+
+    def get_intervals(self, levels):
+
+        intervals = {}
 
         start = min(self.signal[1])  
         end = max(self.signal[1])
         delta = end - start
-        delta = round(delta / float(self.num_of_levels.get()), 2) 
+        delta = round(delta / float(levels), 3) 
 
-        end = round(start + delta, 2)
-        for i in range(int(self.num_of_levels.get())):  
-            x = (start + end) / 2
-            self.intervals[(start, end)] = round(x, 2)
+        end = round(start + delta, 3)
+        for i in range(int(levels)):  
+            intervals[i+1] = (start, end)
             start = end
-            end = round(start + delta, 2)
+            end = round(start + delta, 3)
+
+        return intervals
 
 
-    def quantize_values(self):
-        values = []
-        for element in self.signal[1]:
-            for start, end in self.intervals.keys():
+
+
+    def quantize_values(self, intervals):
+        quantized_values = []
+        errors = []
+        for element in self.signal[1]: #y value
+            for start, end in intervals.values(): 
+
+                midpoint = round((start + end) / 2, 3) 
+                self.midpoints.append(midpoint)
+
                 if start <= element <= end:
-                    values.append(self.intervals[(start, end)])
+                    quantized_values.append(midpoint) #mid point
+                    errors.append(round(midpoint - element, 3)) #error
                     break
 
         self.signal[1].clear()
-        self.signal[1].extend(values)
-        return self.signal[1]
+        self.signal[1].extend(quantized_values) #will then be used to plot
+
+        return quantized_values, errors
         
 
     def get_binary_numbers(self, num_of_bits):
-        self.binary_numbers = []
+        binary_numbers = []
         total_numbers = 2 ** num_of_bits
     
         for i in range(total_numbers):
             binary_number = format(i, f'0{num_of_bits}b')
-            self.binary_numbers.append(binary_number)    
+            binary_numbers.append(binary_number)
+
+        return binary_numbers   
     
 
-    def encode_values(self):
+    def encode_values(self, binary_numbers, quantized_values):
         encoded = {}
         i = 0
-        for value in sorted(set(self.signal[1])):
-            encoded[value] = self.binary_numbers[i]
+        for value in sorted(set(self.midpoints)):
+            encoded[value] = [i+1, binary_numbers[i]]  
             i += 1
         
-        new_list = []
-        for value in self.signal[1]:
-            new_list.append(encoded[value]) 
+        encoded_values = []
+        intervals_indecies = []
+        for value in quantized_values:
+            intervals_indecies.append(encoded[value][0]) #interval index
+            encoded_values.append(encoded[value][1]) #interval index in binary
 
-        return new_list
+        return intervals_indecies, encoded_values
 
 
     def plot_signal(self):
@@ -125,6 +184,6 @@ class Task3:
         self.canvas.draw()
 
 
-    def print_resutls(self, encoded_values, quantized_values):
+    def print_resutls(self,intervals_indecies,encoded_values,quantized_values,errors):
         for i in range(len(encoded_values)):
-            print(encoded_values[i], '\t', quantized_values[i])
+            print('\t', intervals_indecies[i], '\t',  encoded_values[i], '\t', quantized_values[i], '\t', errors[i])
